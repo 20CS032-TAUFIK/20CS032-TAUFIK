2650087696f825e1f535ebc2e46bda71f86b0fe0
20CS032-TAUFIK <90304540+20CS032-TAUFIK@users.noreply.github.com>
2021-09-20 20:35:40 +0530

    Update Exercise01_05.java

--- Chapter_01/Exercise01_05.java
+++ Chapter_01/Exercise01_05.java
@@ -1,8 +1,6 @@
 /**
  * 1.5 (Compute expressions) Write a program that displays the result of
- * 9.5 X 4.5 - 2.5 X 3
- * -------------------
- *     45.5 - 3.5
+ *((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5)
  */
 public class Exercise01_05 {
     public static void main(String[] args) {
