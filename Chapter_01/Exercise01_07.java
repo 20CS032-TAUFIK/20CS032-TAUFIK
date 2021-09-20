24878075935a3f314047fba1114207ce2f569711
20CS032-TAUFIK <90304540+20CS032-TAUFIK@users.noreply.github.com>
2021-09-20 20:36:57 +0530

    Update Exercise01_07.java

--- Chapter_01/Exercise01_07.java
+++ Chapter_01/Exercise01_07.java
@@ -1,5 +1,5 @@
 /**
- * 1.7 (Approximate &Pi;) &Pi; can be computed using the following formula:
+ * 1.7 (Approximate pi;)pi ; can be computed using the following formula:
  * p = 4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11+....)
  * Write a program that displays the result of 4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11)
  * and 4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13). Use 1.0 instead of 1 in your
