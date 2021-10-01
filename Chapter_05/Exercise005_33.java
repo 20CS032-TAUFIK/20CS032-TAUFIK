123c1eccf37f09741f97caadb768667110052d6a
20CS032-TAUFIK <90304540+20CS032-TAUFIK@users.noreply.github.com>
2021-10-01 14:04:52 +0530

    Create Exercise005_33.java

---
+++ Chapter_05/Exercise005_33.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Exercise05_33 {
+   public static void main(String[] args) {
+    
+        long millis = System.currentTimeMillis();
+        System.out.println("Current date and time is " +millis);
+       // Date date = new Date() ;
+       // System.out.println("Current date and time is " + date);
+    }
+}
